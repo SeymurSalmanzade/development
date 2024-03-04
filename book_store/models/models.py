12f60322ee7e3cf9a9bab1424c8cec2b207a719f
SeymurSalmanzade <sseymursalmanzadee@gmail.com>
2024-03-04 13:57:46 +0000

    selling books

--- book_store/models/models.py
+++ book_store/models/models.py
@@ -6,9 +6,18 @@ from odoo import models, fields, api
 class book_store(models.Model):
     _name = 'book_store.book_store'
     _description = 'book_store.book_store'
+    genre = fields.Selection(
+        [('mystery','Mystery'),
+         ('fantasy','Fantasy'),
+         ('thriller','Thriller'),
+         ('horror','Horror'),
+         ('romance','Romance'),
+         ('science','Science'),
+         ('historical','Historical'),
+         ('memoir','Memoir')]
+    )
 
-    name = fields.Char()
-    genre = fields.Char()
+    name = fields.Char())
     author = fields.Char()
     value = fields.Monetary(string="Value")
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id, store=True)
