a9ba17c279cbe8aafe5f3e066bc967f95ef4cb0e
SeymurSalmanzade <sseymursalmanzadee@gmail.com>
2024-03-04 14:08:03 +0000

    selling books

--- book_store/models/models.py
+++ book_store/models/models.py
@@ -6,19 +6,20 @@ from odoo import models, fields, api
 class book_store(models.Model):
     _name = 'book_store.book_store'
     _description = 'book_store.book_store'
-    genre = fields.Selection(
-        [('mystery','Mystery'),
-         ('fantasy','Fantasy'),
-         ('thriller','Thriller'),
-         ('horror','Horror'),
-         ('romance','Romance'),
-         ('science','Science'),
-         ('historical','Historical'),
-         ('memoir','Memoir')]
-    )
+    advance_genre = fields.Selection("_get_advence_genre_list")
 
-    name = fields.Char())
+    name = fields.Char()
     author = fields.Char()
     value = fields.Monetary(string="Value")
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id, store=True)
     description = fields.Text()
+
+    def _get_advence_genre_list(self):
+        return [('mystery','Mystery'),
+                ('fantasy','Fantasy'),
+                ('thriller','Thriller'),
+                ('horror','Horror'),
+                ('romance','Romance'),
+                ('science','Science'),
+                ('historical','Historical'),
+                ('memoir','Memoir')]
