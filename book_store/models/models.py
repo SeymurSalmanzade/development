2be5352dafbe697144e1bbceea4f2b7a791f0cad
SeymurSalmanzade <sseymursalmanzadee@gmail.com>
2024-03-06 12:38:04 +0000

    store

--- book_store/models/models.py
+++ book_store/models/models.py
@@ -5,15 +5,9 @@ from odoo import models, fields, api
 
 class book_store(models.Model):
     _name = 'book_store.book_store'
-    _description = 'book_store.book_store'
+    _description = 'This is a book store'
+    
     genre = fields.Selection("_get_genre_list")
-
-    name = fields.Char(default=lambda self: self.env.user.name)
-    author = fields.Char()
-    value = fields.Monetary(string="Value")
-    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.AZN'), store=True)
-    description = fields.Text()
-
     def _get_genre_list(self):
         return [('mystery','Mystery'),
                 ('fantasy','Fantasy'),
@@ -23,6 +17,21 @@ class book_store(models.Model):
                 ('science','Science'),
                 ('historical','Historical'),
                 ('memoir','Memoir')]
+
+    name = fields.Char()
+    author = fields.Char()
+    release_date = fields.Date("Release Date", copy=False, default=fields.Date.today)
+    premiere_night = fields.Datetime(copy=False, default=fields.Datetime.now)
+    value = fields.Monetary(string="Value")
+    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.AZN'), store=True)
+    description = fields.Text()
+
+class book_lists(models.Model):
+    _name = 'book_lists'
+    _description = 'This is the lists of books'
+
+    name = fields.Char()
+
 
 class res_currency(models.Model):
     _inherit = "res.currency"
