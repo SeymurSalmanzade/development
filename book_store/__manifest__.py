1cc4deda03b23f87b6402e3eb72cc598455fe651
SeymurSalmanzade <sseymursalmanzadee@gmail.com>
2024-03-07 08:54:27 +0000

    store

--- book_store/__manifest__.py
+++ book_store/__manifest__.py
@@ -24,6 +24,7 @@ Long description of module's purpose
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
+        'views/view2.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
