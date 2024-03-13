004778d4fe35ce1cbe763d750bf0aa6e67cb1ceb
SeymurSalmanzade <sseymursalmanzadee@gmail.com>
2024-03-13 09:57:02 +0000

    selling books and movies

--- book_store/models/models.py
+++ book_store/models/models.py
@@ -31,14 +31,64 @@ class book_store(models.Model):
 class res_currency(models.Model):
     _inherit = 'res.currency'
 
-    book_store_id = fields.One2many('book_store.book_store','currency_id')
+    book_lists_id = fields.One2many('book_store.book_store','currency_id')
+    movie_lists_id = fields.One2many('movie_lists','currency_id')
 
-class book_lists(models.Model):
-    _name = 'book_lists'
-    _description = 'This is the lists of books'
+class movie_list(models.Model):
+    _name = 'movie_list'
+    _description = 'This is the list of movies'
 
-    name = fields.Char()
-    name = fields.Char()
-    name = fields.Char()
-    name = fields.Char()
-    name = fields.Char()
+    category = fields.Selection("_get_category_list")
+    def _get_category_list(self):
+        return [('mystery','Mystery'),
+                ('fantasy','Fantasy'),
+                ('crime','Crime'),
+                ('thriller','Thriller'),
+                ('horror','Horror'),
+                ('romance','Romance'),
+                ('science','Science'),
+                ('blu-ray','Blu-Ray'),
+                ('western','Western'),
+                ('historical','Historical'),
+                ('documentary','Documentary'),
+                ('memoir','Memoir')]
+
+    name = fields.Char(copy=False, translate=True)
+    is_new = fields.Boolean("New?", default=False, copy=False, required=True)
+    author = fields.Char(copy=False, size=15)
+    release_date = fields.Date("Release Date", copy=False, default=fields.Date.today)
+    premiere_night = fields.Datetime(copy=False, default=fields.Datetime.now)
+    value = fields.Monetary(string="Value", copy=False)
+    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.AZN'), store=True, copy=False)
+    description = fields.Text(copy=False, translate=True, help='Write some words for selling if you want')
+
+
+# class genre_list(models.Model)
+#     _name = 'genre_list'
+#     _description = 'This is the genre list'
+
+#     name = fields.Char()
+    # fantasy = fields.Char("Fantasy")
+    # thriller = fields.Char("Thriller")
+    # horror = fields.Char("Horror")
+    # romance = fields.Char("Romance")
+    # science = fields.Char("Science")
+    # historical = fields.Char("Historical")
+    # memoir = fields.Char("Memoir")
+
+# class category_list(models.Model)
+#     _name = 'category_list'
+#     _description = 'This is the category list'
+
+#     name = fields.Char()
+    # fantasy = fields.Char("Fantasy")
+    # crime = fields.Char("Crime")
+    # thriller = fields.Char("Thriller")
+    # horror = fields.Char("Horror")
+    # romance = fields.Char("Romance")
+    # science = fields.Char("Science")
+    # blu-Ray = fields.Char("Blu-Ray")
+    # western = fields.Char("Western")
+    # historical = fields.Char("Historical")
+    # documentary = fields.Char("Documentary")
+    # memoir = fields.Char("Memoir")
